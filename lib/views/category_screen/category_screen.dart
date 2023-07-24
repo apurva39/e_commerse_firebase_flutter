4459339e376a7e6a319307738fcf42fd13b2b533
Apurva Anand <apurvaanand101@gmail.com>
2023-07-25 00:09:12 +0530

    screen+controller

---
+++ lib/views/category_screen/category_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../constants/colors.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: backGroundColor,
+    );
+  }
+}
